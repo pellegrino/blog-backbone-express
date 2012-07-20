5cb19fde405bee07e4ec09d14db6237f618c2f22
Vitor Pellegrino <vitorp@gmail.com>
2012-07-20 13:11:16 -0300

    Adding backbone 0.9.2

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,2 +1,3 @@
-//= require plugins 
+//= require backbone
+//= require plugins
 //= require script 
---
+++ assets/js/backbone.js
@@ -0,0 +1,2 @@
+//= require libs/underscore-1.3.3-min.js
+//= require libs/backbone-0.9.2.min.js
